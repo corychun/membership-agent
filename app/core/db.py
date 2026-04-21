e5d843234938067d9a9d48b5ce2d2000582daeee
corychun <jingzhong011290@gmail.com>
2026-04-22 02:45:28 +0800

    Update db.py

--- app/core/db.py
+++ app/core/db.py
@@ -1,20 +1,18 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL not set")
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
-)
-
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
