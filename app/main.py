838b699c5715dc9a26a8d9e382a450b0b7156eaf
corychun <jingzhong011290@gmail.com>
2026-04-26 02:50:12 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
 from app.core.db import Base, engine
 
 from app.api.orders import router as orders
@@ -24,6 +28,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+BASE_DIR = Path(__file__).resolve().parent
+STATIC_DIR = BASE_DIR / "static"
+
+app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
 
 
 @app.get("/")
